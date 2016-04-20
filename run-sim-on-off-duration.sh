871d8612552217207253661d4f41d899ae18ab00
Ganesh Kumar Velu Rajendran <ganz@triangle.cs.wisc.edu>
2016-04-20 12:07:13 -0500

    Added some status echos

--- run-sim-on-off-duration.sh
+++ run-sim-on-off-duration.sh
@@ -4,7 +4,7 @@ echo Running Simulations...
 # Values for the parameters
 NWIFI=5
 TYPE=true   # true for On/Off and false for Poisson
-RATE=30
+RATE=15
 
 # Vary the duration values
 for DURATION in 0.001 0.005 0.01 0.05 0.1 0.5 1
@@ -13,10 +13,11 @@ do
 	FOLDER=$NWIFI\_$TYPE\_$RATE\_$DURATION
 	# Create the folder if it does not exist
 	mkdir -p $FOLDER
-
+	echo "Running for Duration - $DURATION"
 	# Run the simulation code 100 times
 	for i in `seq 1 100`;
 	do 
+		echo "Running Test $i..."
 		WAFCMD="./waf --run \"examples/wireless/wifi-udp --nWifi=$NWIFI --type=$TYPE --rate=$RATE --duration=$DURATION\" > $FOLDER/$NWIFI\_$TYPE\_$RATE\_$DURATION\_$i.dat"
 		eval $WAFCMD
 	done
